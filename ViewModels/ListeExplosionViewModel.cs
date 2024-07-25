e701ede9a0582024a7cda6c548ba8bfd5d5c2fbf
Nepta_ <lucas.besson@outlook.fr>
2024-07-25 14:56:40 +0200

    Update ListeExplosionViewModel.cs

--- ViewModels/ListeExplosionViewModel.cs
+++ ViewModels/ListeExplosionViewModel.cs
@@ -1,11 +1,29 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
-using System.ComponentModel;
+﻿using Explosion.Context.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.ObjectModel;
 
 namespace Explosion.ViewModels
 {
-    public partial class ListeExplosionViewModel(IDialogService dialogService, INavigationService navigationService) : BaseViewModel(dialogService, navigationService)
+    public partial class ListeExplosionViewModel : BaseViewModel
     {
+        private readonly TpExplosionContext _dbContext;
 
+        [ObservableProperty]
+        private ObservableCollection<Explosion.Context.Models.Explosion>? _explosions;
+
+        public ListeExplosionViewModel(IDialogService dialogService, INavigationService navigationService) : base(dialogService, navigationService)
+        {
+            // Remplacez cela par votre logique de récupération de données à partir de Explosion.Context
+            using var context = new TpExplosionContext();
+            _dbContext = context;
+            //Explosions = new ObservableCollection<Explosion.Context.Models.Explosion>(context.Explosions.ToList());
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            Explosions = new ObservableCollection<Explosion.Context.Models.Explosion>(_dbContext.Explosions.ToList());
+        }
     }
 }
