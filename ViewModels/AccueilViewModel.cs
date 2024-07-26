206c5bc48b5e50fa1b49da0a4f25e985321cf879
Alizee <ali_bernard@hotmail.fr>
2024-07-26 10:16:39 +0200

    Accueil

--- ViewModels/AccueilViewModel.cs
+++ ViewModels/AccueilViewModel.cs
@@ -1,11 +1,20 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System.ComponentModel;
+using System.Diagnostics;
 
 namespace Explosion.ViewModels
 {
     public partial class AccueilViewModel(IDialogService dialogService, INavigationService navigationService) : BaseViewModel(dialogService, navigationService)
     {
+        [RelayCommand]
+        async Task Buttonlisteexplosion() => await NavigationService.GoToAsync("///listeexplosions");
 
+        [RelayCommand]
+        async Task Buttonlistepays() => await NavigationService.GoToAsync("///listepays");
+
+        [RelayCommand]
+        private void Buttonquitter() => App.Current!.Quit();
     }
 }
--- Views/AccueilPage.xaml.cs
+++ Views/AccueilPage.xaml.cs
@@ -2,9 +2,12 @@
 {
     public partial class AccueilPage : ContentPage
     {
-        public AccueilPage()
-        {
-            InitializeComponent();
+            public AccueilPage(AccueilViewModel viewModel)
+            {
+                InitializeComponent();
+                viewModel.Title = "Accueil";
+                BindingContext = viewModel;
+                SetBinding(Page.TitleProperty, new Binding(nameof(AccueilViewModel.Title)));
+            }
         }
     }
-}
